6784f1cb8d51c507e344b9a773bd7aff24c53857
Thawanrat Khumsa <ThawanratKhumsa@gmail.com>
2022-11-12 19:14:09 +0700

    add image

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Boots',
-            description: 'รองเท้าบูทส์'
+            description: 'รองเท้าบูทส์',
+            image: './assets/image/socks_green.jpg'
         }
     }
 })
