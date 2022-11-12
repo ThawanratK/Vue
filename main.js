db78478c46a7eac85871aa0ff27c4fdf2e240b68
Thawanrat Khumsa <ThawanratKhumsa@gmail.com>
2022-11-12 21:17:39 +0700

    add compute var

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ const app = Vue.createApp({
     data() {
         return {
             cart: 0,
-            product: 'Boots',
+            brand: 'Vue Mastery',
+            product: 'Socks',
             description: 'รองเท้าบูทส์',
-            image: './assets/images/socks_green.jpg',
-            onSale: false,
-            inStock: true,
+           // image: './assets/images/socks_green.jpg',
+            onSale: true,
+           // inStock: true,
+            selectedVariant: 0,
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue',  image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue',  image: './assets/images/socks_blue.jpg', quantity: 0 }
             ]
 
         }
@@ -25,6 +27,24 @@ const app = Vue.createApp({
         },
         changeImage(currentImage){
             this.image = currentImage
+        },
+        updateVariant(index){
+            this.selectedVariant = index
         }
     },
+    computed:{
+        title(){
+            if(this.onSale){
+                return this.brand+ ' '+ this.product+' is on SALE!!'
+            }else{
+                return this.brand+ ' '+ this.product
+            }
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        }
+    }
 })
