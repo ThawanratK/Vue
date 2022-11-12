945016ae00c9ec9b2535a7cc8ecbb42347fed538
Thawanrat Khumsa <ThawanratKhumsa@gmail.com>
2022-11-12 20:37:23 +0700

    add event

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Boots',
             description: 'รองเท้าบูทส์',
             image: './assets/images/socks_green.jpg',
-            OnSale: false,
+            onSale: false,
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue',  image: './assets/images/socks_blue.jpg' }
             ]
 
         }
-    }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        removeFromCart(){
+            if(this.cart != 0){
+               this.cart -= 1 
+            }
+        },
+        changeImage(currentImage){
+            this.image = currentImage
+        }
+    },
 })
